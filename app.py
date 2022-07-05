878e078f08b10f03f194306a773bea4794742070
SK <sarthakkarandikar03@gmail.com>
2022-07-05 17:27:40 +0530

    changes

--- app.py
+++ app.py
@@ -18,35 +18,5 @@ def video_feed():
 def capture_page():
     return render_template("capture.html")
 
-#@app.route('/captur')
-#def recognize_emotion():
-    # initialize the camera
-    # If you have multiple camera connected with 
-    # current device, assign a value in cam_port 
-    # variable according to that
-    cam_port = 0
-    cam = VideoCapture(cam_port)
-    
-    # reading the input using the camera
-    result, image = cam.read()
-    
-    # If image will detected without any error, 
-    # show result
-    if result:
-    
-        # showing result, it takes frame name and image 
-        # output
-        imshow("GeeksForGeeks", image)
-    
-    
-        # If keyboard interrupt occurs, destroy image 
-        # window
-        waitKey(0)
-        destroyWindow("GeeksForGeeks")
-    
-    # If captured image is corrupted, moving to else part
-    else:
-        print("No image detected. Please! try again")
-
 if __name__=="__main__":
     app.run(debug=True, port=5000)
--- camera.py
+++ camera.py
@@ -1,16 +1,23 @@
-from cv2 import *
+import cv2
+import fer
+import pandas
+import numpy
+import matplotlib
 
 class VideoCamera(object):
     def __init__(self):
-        self.video=VideoCapture(0)
+        self.video=cv2.VideoCapture(0)
 
     def __del__(self):
-        self.video.releast(0)
+        self.video.releast()
 
     def get_frame(self):
         ret,frame=self.video.read()
-        ret,jpeg=imencode('.jpg',frame)
+        ret,jpeg=cv2.imencode('.jpg',frame)
         return jpeg.tobytes()
+
+    def recognize_emotions(self):
+        
 
     
         
