e4a2918e68312753339fe147eb17d4bf4311dec6
SK <sarthakkarandikar03@gmail.com>
2022-07-05 16:17:56 +0530

    added camera

---
+++ camera.py
@@ -0,0 +1,15 @@
+from cv2 import *
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video=VideoCapture(0)
+
+    def __del__(self):
+        self.video.releast(0)
+
+    def get_frame(self):
+        ret,frame=self.video.read()
+        ret,jpeg=imencode('.jpg',frame)
+
+    
+        
