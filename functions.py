ca0f4ae03a0c084be720cb1b3bf779234469b817
SK <sarthakkarandikar03@gmail.com>
2022-07-05 18:39:18 +0530

    trying date time module

--- functions.py
+++ functions.py
@@ -2,9 +2,11 @@ from calendar import EPOCH
 from camera import *
 from cv2 import *
 from time import *
+from datetime import *
 
 
 def generate_camera(camera):
-    frame = camera.get_frame()
-    yield(b'--frame\r\n' b'Content-Type:image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
+    end_time = datetime.now() + timedelta(seconds=5)
+    while datetime.now() < end_time:
+        frame = camera.get_frame()
+        yield (b"--frame\r\n" b"Content-Type:image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
